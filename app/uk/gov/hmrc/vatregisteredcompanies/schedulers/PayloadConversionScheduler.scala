39555e512790866eb9e6633e95126e5f62d5f65a
Matt Roberts <mattjr72@gmail.com>
2019-03-11 17:17:17 +0000

    injecting schedule interval, and logging

--- app/uk/gov/hmrc/vatregisteredcompanies/schedulers/PayloadConversionScheduler.scala
+++ app/uk/gov/hmrc/vatregisteredcompanies/schedulers/PayloadConversionScheduler.scala
@@ -18,35 +18,50 @@ package uk.gov.hmrc.vatregisteredcompanies.schedulers
 
 import java.util.concurrent.TimeUnit
 
-import akka.actor.ActorSystem
-import com.google.inject.AbstractModule
-import javax.inject.{Inject, Singleton}
-import play.api.Logger
+import akka.actor.{ActorSystem, Cancellable}
+import com.google.inject.{AbstractModule, Provides}
+import javax.inject.{Inject, Named, Singleton}
+
+import play.api.Mode.Mode
+import play.api.{Configuration, Environment, Logger}
+import uk.gov.hmrc.play.config.ServicesConfig
 import uk.gov.hmrc.vatregisteredcompanies.services.PersistenceService
 
-import scala.concurrent.ExecutionContext
-import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.duration.{Duration, FiniteDuration}
 
 @Singleton
-class PayloadConversionScheduler @Inject()(persistenceService: PersistenceService, actorSystem: ActorSystem)(
+class PayloadConversionScheduler @Inject()(
+  persistenceService: PersistenceService,
+  actorSystem: ActorSystem,
+  @Named("interval") interval: FiniteDuration)(
   implicit val ec: ExecutionContext) {
 
   private val logger = Logger(getClass)
 
-  // TODO make configurable
-  val interval = FiniteDuration(30, TimeUnit.SECONDS)
-
   logger.info(s"Initialising update every $interval")
 
+  // TODO port to Actor to avoid job stopping on failure or overlapping
   actorSystem.scheduler.schedule(FiniteDuration(10, TimeUnit.SECONDS), interval) {
-    logger.info("Scheduling a mongo counter")
-    persistenceService.processData
+    logger.info("Scheduling inbound data processing")
+    persistenceService.processData.recover {
+      case e: RuntimeException => Logger.error(s"Error processing vat registration data: $e")
+    }
   }
 
 }
 
-class PayloadConversionSchedulerModule extends AbstractModule {
+class PayloadConversionSchedulerModule(environment: Environment, val runModeConfiguration: Configuration) extends
+  AbstractModule with ServicesConfig {
+  override protected def mode: Mode = environment.mode
+
+  @Provides
+  @Named("interval")
+  def interval(): FiniteDuration =
+    new FiniteDuration(getConfInt("schedulers.payload.conversion.interval.seconds", 600).toLong, TimeUnit.SECONDS)
+
   override def configure(): Unit = {
     bind(classOf[PayloadConversionScheduler]).asEagerSingleton()
   }
+
 }
