26df302378694f41223979b8548f95e37302b0a2
Matt Roberts <mattrobertsky@users.noreply.github.com>
2019-06-21 11:34:18 +0100

    updating driver

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile = Seq(
 
     "uk.gov.hmrc"             %% "play-reactivemongo"       % "6.4.0",
-    "uk.gov.hmrc"             %% "simple-reactivemongo"     % "7.12.0-play-25",
+    "uk.gov.hmrc"             %% "simple-reactivemongo"     % "7.20.0-play-25",
     "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.8.0",
     "org.typelevel"           %% "cats-core"                % "1.1.0",
     "com.github.fge"          %  "json-schema-validator"    % "2.2.6",
