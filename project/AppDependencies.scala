20391865c12335194aca39b7cbb80ab025eeb7bb
jackwillett1 <jack.willett1@digital.hmrc.gov.uk>
2022-10-25 09:09:00 +0100

    Play library upgraded

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,8 +5,8 @@ object AppDependencies {
 
   val compile = Seq(
 
-    "uk.gov.hmrc"             %% "simple-reactivemongo"      % "8.0.0-play-28",
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28" % "5.7.0",
+    "uk.gov.hmrc"             %% "simple-reactivemongo"      % "8.1.0-play-28",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28" % "7.8.0",
     "org.typelevel"           %% "cats-core"                 % "2.4.2",
     "com.github.fge"          %  "json-schema-validator"     % "2.2.6",
     "com.typesafe.akka"       %% "akka-stream"               % "2.5.18",
@@ -15,14 +15,14 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.7.0"             % "test",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "7.8.0"             % "test",
     "org.scalatest"           %% "scalatest"                % "3.2.9"             % "test",
     "org.scalacheck"          %% "scalacheck"               % "1.14.0"            % "test",
     "com.typesafe.play"       %% "play-test"                % current             % "test",
     "org.mockito"             %  "mockito-core"             % "3.11.2"            % "test",
     "org.scalatestplus"       %% "mockito-3-4"              % "3.2.9.0"           % "test, it",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"             % "test, it",
-    "uk.gov.hmrc"             %% "service-integration-test" % "1.1.0-play-28"     % "test, it",
+    "uk.gov.hmrc"             %% "service-integration-test" % "1.3.0-play-28"     % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"             % "test, it",
     "org.scalatestplus"       %% "scalacheck-1-14"          % "3.2.0.0"           % "test, it",
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"            % "test, it"
