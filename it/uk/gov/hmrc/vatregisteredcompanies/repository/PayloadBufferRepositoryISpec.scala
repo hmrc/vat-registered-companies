73a205a3f6e6bf24691ba9e07e5db1143d643246
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2022-11-01 13:01:15 +0000

    add insert tests to PBRiSpec
    update RBPiS tests to pass
    
    DLS-6037-BR-update insert methods to use actual repo
    
    DLS-6037 BR - fix method list tests
    
    update tests
    
    DLS-6037-add lock repo operation call to isb
    
    DLS-6037-sync testData with Graemes
    
    update to match graeme
    
    adding countLock to db operations and updating tests
    
    DLS-6037
    
    update deleteOne first test
    
    added controller tests

--- it/uk/gov/hmrc/vatregisteredcompanies/repository/PayloadBufferRepositoryISpec.scala
+++ it/uk/gov/hmrc/vatregisteredcompanies/repository/PayloadBufferRepositoryISpec.scala
@@ -1,1 +1,208 @@
 
+package uk.gov.hmrc.vatregisteredcompanies.repository
+
+import uk.gov.hmrc.vatregisteredcompanies.helpers.IntegrationSpecBase
+import uk.gov.hmrc.vatregisteredcompanies.helpers.TestData._
+import uk.gov.hmrc.vatregisteredcompanies.repositories.PayloadWrapper
+
+class PayloadBufferRepositoryISpec extends IntegrationSpecBase {
+  override def beforeEach(): Unit = {
+    deleteAllBuffer
+  }
+
+  "Method: deleteAll" when {
+    "there are no records in the database" should {
+      "Have no records" in {
+        bufferTotalCount shouldBe 0
+        val res = payloadBufferRepository.deleteAll()
+
+        whenReady(res) { result =>
+          result shouldBe ((): Unit)
+          bufferTotalCount shouldBe 0
+        }
+      }
+    }
+
+    "there is 1 record in the database" should {
+      "Have one record" in {
+        insertOneBuffer(testPayloadCreateAndUpdates1)
+        bufferTotalCount shouldBe 1
+        val res = payloadBufferRepository.deleteAll()
+
+        whenReady(res) { result =>
+          result shouldBe ((): Unit)
+          bufferTotalCount shouldBe 0
+        }
+      }
+    }
+
+    "there are multiple records in the database" should {
+      "Have two records" in {
+        insertOneBuffer(testPayloadCreateAndUpdates)
+        insertOneBuffer(testPayloadCreateAndUpdates1)
+        bufferTotalCount shouldBe 2
+        val res = payloadBufferRepository.deleteAll()
+
+        whenReady(res) { result =>
+          result shouldBe ((): Unit)
+          bufferTotalCount shouldBe 0
+        }
+      }
+    }
+  }
+
+  "Method: insert" when {
+    "there are no records in the database" should {
+      "Add one record" in {
+        val res = payloadBufferRepository.insert(testPayloadCreateAndUpdates1)
+
+        whenReady(res) { result =>
+          result shouldBe ((): Unit)
+          bufferTotalCount shouldBe 1
+        }
+      }
+    }
+
+    "there are multiple records in the database" should {
+      "Increase the database by one record" in {
+        insertOneBuffer(testPayloadCreateAndUpdates1)
+        insertOneBuffer(testPayloadCreateAndUpdates)
+        bufferTotalCount shouldBe 2
+
+        val res = payloadBufferRepository.insert(testPayloadCreateAndUpdates1)
+
+        whenReady(res) { result =>
+          result shouldBe ((): Unit)
+          bufferTotalCount shouldBe 3
+        }
+      }
+    }
+
+    "there are multiple records in the database" should {
+      "Increase the database by two records" in {
+        insertOneBuffer(testPayloadCreateAndUpdates1)
+        insertOneBuffer(testPayloadCreateAndUpdates)
+        bufferTotalCount shouldBe 2
+
+        val res = {
+          payloadBufferRepository.insert(testPayloadCreateAndUpdates1)
+          payloadBufferRepository.insert(testPayloadCreateAndUpdates)
+        }
+
+        whenReady(res) { result =>
+          result shouldBe ((): Unit)
+          bufferTotalCount shouldBe 4
+        }
+      }
+    }
+  }
+
+  "Method: list" when {
+
+    "there are no records in the database" should {
+      "Have no records" in {
+        val res = {
+          payloadBufferRepository.list
+        }
+
+        whenReady(res) { result =>
+          result shouldBe empty
+          bufferTotalCount shouldBe 0
+        }
+
+      }
+    }
+
+    "there is 1 record in the database" should {
+      "Have one record" in {
+
+        insertOneBuffer(testPayloadCreateAndUpdates)
+        val res = {
+          payloadBufferRepository.list
+        }
+
+        whenReady(res) { result =>
+          result.map(_.payload) shouldBe List(testPayloadCreateAndUpdates)
+          bufferTotalCount shouldBe 1
+        }
+      }
+    }
+
+    "there are multiple records in the database" should {
+      "Have three records" in {
+        insertOneBuffer(testPayloadCreateAndUpdates1)
+        insertOneBuffer(testPayloadCreateAndUpdates)
+        insertOneBuffer(testPayloadCreateAndUpdates1)
+        val res = {
+          payloadBufferRepository.list
+        }
+
+        whenReady(res) { result =>
+
+          result.head.payload shouldBe testPayloadCreateAndUpdates1
+          bufferTotalCount shouldBe 3
+        }
+      }
+    }
+  }
+
+//      "Method: getOne" when {
+//        "there are no records in the database" should {
+//          "Have no records" in {
+//            bufferTotalCount shouldBe 0
+//          }
+//        }
+//
+//        "there is 1 record in the database" should {
+//          "Have one record" in {
+//            insertOne(getVatRegCompany(testVatNo1))
+//            bufferTotalCount shouldBe 0
+//          }
+//        }
+//
+//        "there are multiple records in the database" should {
+//          "Have three records" in {
+//            insertMany(List(getVatRegCompany(testVatNo1), getVatRegCompany(testVatNo2), getVatRegCompany(testVatNo3)))
+//            bufferTotalCount shouldBe 0
+//          }
+//        }
+//      }
+      "Method: deleteOne" when {
+        "there are no records in the database" should {
+          "Have no records" in {
+            bufferTotalCount shouldBe 0
+            val currentRecordsList = {
+              payloadBufferRepository.list
+            }
+            val firstRecord = whenReady(currentRecordsList) { first => first.head.payload }
+            val res = payloadBufferRepository.deleteOne(createPayloadWrapper(testPayloadDeletes))
+
+            whenReady(res) { result =>
+              result shouldBe ((): Unit)
+              bufferTotalCount shouldBe 0
+            }
+        }
+
+        "there is 1 record in the database" should {
+          "Have one record" in {
+            bufferTotalCount shouldBe 1
+            val currentRecordsList = {
+              payloadBufferRepository.list
+            }
+            val res = payloadBufferRepository.deleteOne(createPayloadWrapper(testPayloadDeletes))
+
+            whenReady(res) { result =>
+              result shouldBe ((): Unit)
+              bufferTotalCount shouldBe 0
+            }
+        }
+
+        "there are multiple records in the database" should {
+          "Have three records" in {
+            insertMany(List(getVatRegCompany(testVatNo1), getVatRegCompany(testVatNo2), getVatRegCompany(testVatNo3)))
+            bufferTotalCount shouldBe 0
+          }
+      }
+    }
+  }
+}
---
+++ it/uk/gov/hmrc/vatregisteredcompanies/schedulers/PayloadConversionSchedulerISpec.scala
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vatregisteredcompanies.schedulers
+
+import uk.gov.hmrc.vatregisteredcompanies.helpers.IntegrationSpecBase
+
+class PayloadConversionSchedulerISpec extends IntegrationSpecBase {
+
+  "processOneData" when {
+    "the lock is not already acquired" should {
+      "get the oldest payload from the buffer repository and process it then delete buffer record" when {
+        "there is one record in the buffer and no records in vatRegisteredCompanies" that {
+          "contains a payload with only createsAndUpdates" in {
+            //insert one record into buffer repository that has a payload with only createsAndUpdates
+            val res = persistenceService.processOneData
+
+            whenReady(res) {result =>
+              result shouldBe ((): Unit)
+              //check records inserted into vatRegisteredCompanies database
+              //check buffer record deleted
+              //check lock has been removed
+            }
+          }
+
+          "contains a payload with only deletes" in {
+            //insert one record into buffer repository that has a payload with only deletes
+            val res = persistenceService.processOneData
+
+            whenReady(res) {result =>
+              result shouldBe ((): Unit)
+              //check records deleted from vatRegisteredCompanies database
+              //check buffer record deleted
+              //check lock has been removed
+            }
+          }
+
+          "contains a payload with both createsAndUpdates and deletes" in {
+            //insert one record into buffer repository that has a payload with createAndUpdates and deletes
+            val res = persistenceService.processOneData
+
+            whenReady(res) {result =>
+              result shouldBe ((): Unit)
+              //check records inserted into vatRegisteredCompanies database
+              //check records deleted from vatRegisteredCompanies database
+              //check buffer record deleted
+              //check lock has been removed
+            }
+          }
+        }
+
+        "there is one record in the buffer" that {
+          "contains a payload with only createsAndUpdates" in {
+            //insert one record into buffer repository that has a payload with only createsAndUpdates
+            // insert records into vatRegisteredCompanies with at least one with the same vatNumber in the buffer paylod
+             val res = persistenceService.processOneData
+
+            whenReady(res) {result =>
+              result shouldBe ((): Unit)
+              //check records inserted into vatRegisteredCompanies database
+              //check buffer record deleted
+              //check lock has been removed
+            }
+          }
+
+          "contains a payload with only deletes" in {
+            //insert one record into buffer repository that has a payload with only deletes
+            // insert records into vatRegisteredCompanies with at least one with the same vatNumber in the buffer paylod
+            val res = persistenceService.processOneData
+
+            whenReady(res) {result =>
+              result shouldBe ((): Unit)
+              //check records deleted from vatRegisteredCompanies database
+              //check buffer record deleted
+              //check lock has been removed
+            }
+          }
+
+          "contains a payload with both createsAndUpdates and deletes" in {
+            //insert one record into buffer repository that has a payload with createAndUpdates and deletes
+            // insert records into vatRegisteredCompanies with at least one with the same vatNumber in the buffer paylod
+            val res = persistenceService.processOneData
+
+            whenReady(res) {result =>
+              result shouldBe ((): Unit)
+              //check records inserted into vatRegisteredCompanies database
+              //check records deleted from vatRegisteredCompanies database
+              //check buffer record deleted
+              //check lock has been removed
+            }
+          }
+        }
+
+        "there is multiple records in the buffer" that {
+          "has the oldest record containing a payload with only createsAndUpdates" in {
+            //insert one record into buffer repository that has a payload with only createsAndUpdates
+            //insert one record into buffer repository that has a payload with createAndUpdates and deletes
+            //insert one record into buffer repository that has a payload with only deletes
+            val res = persistenceService.processOneData
+
+            whenReady(res) {result =>
+              result shouldBe ((): Unit)
+              //check records inserted into vatRegisteredCompanies database
+              //check buffer record with payload containing only deletes is deleted
+              //check lock has been removed
+            }
+          }
+
+          "has a oldest record containing a payload with only deletes" in {
+            //insert one record into buffer repository that has a payload with only deletes
+            //insert one record into buffer repository that has a payload with only createsAndUpdates
+            //insert one record into buffer repository that has a payload with createAndUpdates and deletes
+            // insert records in vatRegisteredCompanies to be deleted
+            val res = persistenceService.processOneData
+
+            whenReady(res) {result =>
+              result shouldBe ((): Unit)
+              //check records deleted from vatRegisteredCompanies database
+              //check buffer record deleted with payload containing only deletes
+              //check lock has been removed
+            }
+          }
+
+          "has a oldest record containing a payload with both createsAndUpdates and deletes" in {
+            //insert one record into buffer repository that has a payload with createAndUpdates and deletes
+            //insert one record into buffer repository that has a payload with only deletes
+            //insert one record into buffer repository that has a payload with only createsAndUpdates
+            //insert records in vatRegisteredCompanies to be deleted
+            val res = persistenceService.processOneData
+
+            whenReady(res) {result =>
+              result shouldBe ((): Unit)
+              //check records inserted into vatRegisteredCompanies database
+              //check records deleted from vatRegisteredCompanies database
+              //check buffer record deleted with payload containing createsAndUpdates and deletes
+              //check lock has been removed
+            }
+          }
+        }
+      }
+    }
+
+    "the lock is already acquired" that {
+      "is within the TTL" should {
+        "not run the processing job and return unit" in {
+          //insert one record into buffer repository that has a payload with createAndUpdates and deletes
+          //insert records in vatRegisteredCompanies to be deleted
+          // insert a lock within TTL
+          val res = persistenceService.processOneData
+
+          whenReady(res) {result =>
+            result shouldBe ((): Unit)
+            //check no records inserted into vatRegisteredCompanies database
+            //check no records deleted from vatRegisteredCompanies database
+            //check buffer record is still present with payload containing createsAndUpdates and deletes
+            //check lock is still present removed
+          }
+
+        }
+      }
+
+      "is outside of the TTL" should {
+        "not run the processing job, remove the lock and return unit" in {
+          //insert one record into buffer repository that has a payload with createAndUpdates and deletes
+          //insert records in vatRegisteredCompanies to be deleted
+          // insert a lock within TTL
+          val res = persistenceService.processOneData
+
+          whenReady(res) {result =>
+            result shouldBe ((): Unit)
+            //check no records inserted into vatRegisteredCompanies database
+            //check no records deleted from vatRegisteredCompanies database
+            //check buffer record is still present with payload containing createsAndUpdates and deletes
+            //check lock has been removed
+          }
+        }
+      }
+    }
+  }
+}
