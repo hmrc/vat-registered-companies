dbd82a527280a916f7262321856e7d9d1316f738
Christine Stokes <18244373+Chrissystk@users.noreply.github.com>
2022-11-03 15:19:20 +0000

    added controller tests

--- it/uk/gov/hmrc/vatregisteredcompanies/repository/LockRepositoryISpec.scala
+++ it/uk/gov/hmrc/vatregisteredcompanies/repository/LockRepositoryISpec.scala
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.gov.hmrc.vatregisteredcompanies.repository
 import uk.gov.hmrc.vatregisteredcompanies.helpers.IntegrationSpecBase
 import uk.gov.hmrc.vatregisteredcompanies.helpers.TestData._
