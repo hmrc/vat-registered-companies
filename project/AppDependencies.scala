ca7b158d872591afe6c08af8e1705031bc5ddc64
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2022-11-14 12:15:43 +0000

    DLS-5551-update AppDependencies to use play-mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,16 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "8.0.0-play-28",
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.7.0",
-    "org.typelevel" %% "cats-core" % "2.4.2",
-    "com.github.fge" % "json-schema-validator" % "2.2.6",
-    "com.typesafe.akka" %% "akka-stream" % "2.5.18",
-    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.5" cross CrossVersion.full),
+
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"        % "0.68.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28" % "5.7.0",
+    "org.typelevel"           %% "cats-core"                 % "2.4.2",
+    "com.github.fge"          %  "json-schema-validator"     % "2.2.6",
+    "com.typesafe.akka"       %% "akka-stream"               % "2.5.18",
+    compilerPlugin("com.github.ghik"           % "silencer-plugin" % "1.7.5" cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % "1.7.5" % Provided cross CrossVersion.full
   )
+
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.7.0"             % "test",
     "org.scalatest"           %% "scalatest"                % "3.2.9"             % "test",
@@ -23,8 +25,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "service-integration-test" % "1.1.0-play-28"     % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"             % "test, it",
     "org.scalatestplus"       %% "scalacheck-1-14"          % "3.2.0.0"           % "test, it",
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"            % "test, it",
-    "com.github.tomakehurst"   % "wiremock-jre8-standalone"    % "2.33.2"    % "test, it"
-
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"            % "test, it"
   )
 }
+
