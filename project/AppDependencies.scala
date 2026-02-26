a018d59e9b98a6395a7a1cf9c9199992db486f52
ZaraFarzan <177644563+ZaraFarzan@users.noreply.github.com>
2026-02-26 14:36:25 +0000

    DLS-12759:[VAT] Upgrade to bootstrap-play (10.6.0)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   val pekkoSite = "org.apache.pekko"
   val pekkoVersion = "1.3.0" // Do not update as this may cause a Bobby Violation
   val playVersion = "play-30"
-  val bootstrapVersion = "9.19.0"
+  val bootstrapVersion = "10.6.0"
   val hmrcMongoVersion = "2.11.0"
 
   val compile = Seq(
